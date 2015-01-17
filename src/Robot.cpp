9a3b683f7e87e0402d022d5ff8a1f9a7f1dc08e9
NickBarkhuff <npbarkhuff@gmail.com>
2015-01-17 15:56:32 -0800

    Changes to encoder and SmartDashboard things

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -9,11 +9,11 @@ public:
 	Robot():
 		motor(0),
 		button(0),
-		encoder(1,2)
+		encoder(1, 2, false, Encoder::k4X)
 		{
 			encoder.SetSamplesToAverage(5);
-			encoder.SetDistancePerPulse(1);
-			encoder.SetMinRate(1);
+			encoder.SetDistancePerPulse(1.0 / 360.0 * 2.0 * 3.1415 * 1.5);
+			encoder.SetMinRate(1.0);
 		}
 	
 	void RobotInit(){
@@ -34,14 +34,15 @@ public:
 
 	void TeleopPeriodic(){
 		if(button.Get() == true){
-			motor.Set(0.2);
+			motor.Set(0.5);
 		}else{
 			motor.Set(0);
 		}
 
+		SmartDashboard::PutBoolean("Button Pushed", button.Get());
+		SmartDashboard::PutBoolean("Encoder Stopped", encoder.GetStopped());
 		SmartDashboard::PutNumber("Encoder Rate", encoder.GetRate());
 		SmartDashboard::PutNumber("Encoder Distance", encoder.GetDistance());
-		SmartDashboard::PutBoolean("Encoder Stopped", encoder.GetStopped());
 	}
 };
 
