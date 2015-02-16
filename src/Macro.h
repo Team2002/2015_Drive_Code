cc0de45d40a7afb055741d753894a4b74ead12b2
= <=>
2015-02-16 11:14:48 -0800

    Split everything up into their own classes.

--- src/Macro.h
+++ src/Macro.h
@@ -1,20 +1,23 @@
+#ifndef MACRO_H__INCLUDED
+#define MACRO_H__INCLUDED
+
+
 #include "WPILib.h"
-#include "RobotMap.h"
-#include <iostream>
-#include <string>
-#include <cstdio>
+#include "Drive.h"
+#include "Lift.h"
+#include "Intake.h"
 
 
-class MacroRecorder{
+class Macro{
 public:
-	MacroRecorder();
-	~MacroRecorder();
-
+	Macro();
+	~Macro();
+	
 	void StartRecording(bool);
-	void SaveStep(float, float, int, int, int, int, int, int);
+	void SaveStep(float, float, bool, bool, bool, int, int);
 	void StopRecording(void);
-
-	void Play(const int, Talon*[NUMBER_OF_TALONS], DoubleSolenoid*[NUMBER_OF_SOLENOIDS]);
+	
+	void Play(const int, Drive*, Lift*, Intake*, Compressor*);
 	
 private:
 	Timer* StepTimer;
@@ -23,135 +26,4 @@ private:
 };
 
 
-MacroRecorder::MacroRecorder(){
-	StepTimer = new Timer();
-	File = new FILE();
-	is_recording = false;
-}
-
-
-MacroRecorder::~MacroRecorder(){
-	delete StepTimer;
-	delete File;
-}
-
-
-void MacroRecorder::StartRecording(bool start = true){
-	if(is_recording || !start) return;
-	is_recording = true;
-
-	int log_file = 0;
-
-	while(true){
-		File = fopen(("/home/lvuser/" + std::to_string(log_file) + ".csv").c_str(), "r");
-
-		if(File == NULL)
-			break;
-		else
-			log_file++;
-	}
-
-	File = fopen(("/home/lvuser/" + std::to_string(log_file) + ".csv").c_str(), "w");
-
-	StepTimer->Reset();
-	StepTimer->Start();
-
-	return;
-}
-
-
-void MacroRecorder::SaveStep(float left_speed, float right_speed, int left_intake_state, int right_intake_state, int lift_state, int claw_state, int intake_state, int door_state){
-	if(!is_recording) return;
-	
-	StepTimer->Stop();
-	
-	fprintf(File, "%f,%f,%f,%d,%d,%d,%d,%d,%d\n", StepTimer->Get(), left_speed, right_speed, left_intake_state, right_intake_state, lift_state, claw_state, intake_state, door_state);
-	
-	StepTimer->Reset();
-	StepTimer->Start();
-}
-
-
-void MacroRecorder::StopRecording(void){
-	if(!is_recording) return;
-	is_recording = false;
-
-	StepTimer->Stop();
-
-	fclose(File);
-}
-
-
-void MacroRecorder::Play(const int file_number, Talon* Talons[NUMBER_OF_TALONS], DoubleSolenoid* Solenoids[NUMBER_OF_SOLENOIDS]){
-	if(is_recording) return;
-
-	File = fopen(("/home/lvuser/" + std::to_string(file_number) + ".csv").c_str(), "r");
-	if(File == NULL) return;
-
-	float time, left_speed, right_speed;
-	int left_intake_state, right_intake_state, lift_state, claw_state, intake_state, door_state;
-	
-	while(true){
-		if(fscanf(File, "%f,%f,%f,%d,%d,%d,%d,%d,%d\n", &time, &left_speed, &right_speed, &left_intake_state, &right_intake_state, &lift_state, &claw_state, &intake_state, &door_state) < 6)
-			break;
-
-		Wait(time);
-
-		for(int i = 0;i < NUMBER_OF_TALONS - 2;i++){
-			if(i < (NUMBER_OF_TALONS - 2) / 2)
-				Talons[i]->Set(right_speed);
-			else
-				Talons[i]->Set(left_speed);
-		}
-
-		if(left_intake_state == 0)
-			Talons[PORT_LEFT_INTAKE_TALON]->Set(0);
-		if(left_intake_state == 1)
-			Talons[PORT_LEFT_INTAKE_TALON]->Set(INTAKE_SPEED);
-		if(left_intake_state == 2)
-			Talons[PORT_LEFT_INTAKE_TALON]->Set(-INTAKE_SPEED);
-
-		if(right_intake_state == 0)
-			Talons[PORT_RIGHT_INTAKE_TALON]->Set(0);
-		if(right_intake_state == 1)
-			Talons[PORT_RIGHT_INTAKE_TALON]->Set(INTAKE_SPEED);
-		if(right_intake_state == 2)
-			Talons[PORT_RIGHT_INTAKE_TALON]->Set(-INTAKE_SPEED);
-
-		if(lift_state == 0){
-			Solenoids[0]->Set(DoubleSolenoid::kOff);
-		}else if(lift_state == 1){
-			Solenoids[0]->Set(DoubleSolenoid::kForward);
-		}else if(lift_state == 2){
-			Solenoids[0]->Set(DoubleSolenoid::kReverse);
-		}
-
-		if(claw_state == 0){
-			Solenoids[1]->Set(DoubleSolenoid::kOff);
-		}else if(claw_state == 1){
-			Solenoids[1]->Set(DoubleSolenoid::kForward);
-		}else if(claw_state == 2){
-			Solenoids[1]->Set(DoubleSolenoid::kReverse);
-		}
-
-		if(door_state == 0){
-			Solenoids[2]->Set(DoubleSolenoid::kOff);
-		}else if(door_state == 1){
-			Solenoids[2]->Set(DoubleSolenoid::kForward);
-		}else if(door_state == 2){
-			Solenoids[2]->Set(DoubleSolenoid::kReverse);
-		}
-	}
-
-	for(int i = 0;i < NUMBER_OF_TALONS;i++){
-		Talons[i]->Set(0);
-	}
-
-	for(int i = 0;i < NUMBER_OF_SOLENOIDS;i++){
-		Solenoids[i]->Set(DoubleSolenoid::kOff);
-	}
-
-	fclose(File);
-
-	return;
-}
+#endif /* MACRO_H__INCLUDED */
--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,189 +1,91 @@
 #include "WPILib.h"
+#include "Robot.h"
+#include "Constants.h"
 #include "Macro.h"
-
-class Robot: public SampleRobot{
-public:
-	Joystick* Joysticks[NUMBER_OF_JOYSTICKS];       // 0 = Drive Joystick, 1 = Lift Joystick
-	Talon* Talons[NUMBER_OF_TALONS];                // 0-1 = Intake Talons, 2-3 = Left Talons, 4-5 = Right Talons
-	DoubleSolenoid* Solenoids[NUMBER_OF_SOLENOIDS]; // 0 = Lift, 1 = Claw, 2 = Intake, 3 = Door
-	Timer* Timers[NUMBER_OF_SOLENOIDS];             // 0 = Lift Timer, 1 = Claw Timer, 2 = Intake Timer, 3 = Door TImer
-	Compressor* AirCompressor;                      // Air compressor
-	MacroRecorder* Macro;                           // Record and play back macros
-
-	Robot();
-	~Robot();
-
-	void RobotInit();       // Called when the program loads
-	void Autonomous();      // Called on start of autonomous
-	void OperatorControl(); // Called on start of tele-op
-};
 
 
 Robot::Robot(){
-	Joysticks[0] = new Joystick(PORT_JOYSTICK_1);
-	Joysticks[1] = new Joystick(PORT_JOYSTICK_2);
-	Talons[0] = new Talon(PORT_LEFT_INTAKE_TALON);
-	Talons[1] = new Talon(PORT_RIGHT_INTAKE_TALON);
-	Talons[2] = new Talon(PORT_LEFT_TALON_1);
-	Talons[3] = new Talon(PORT_LEFT_TALON_2);
-	Talons[4] = new Talon(PORT_RIGHT_TALON_1);
-	Talons[5] = new Talon(PORT_RIGHT_TALON_2);
-	Solenoids[0] = new DoubleSolenoid(PORT_1_LIFT_SOLENOID, PORT_2_LIFT_SOLENOID);
-	Solenoids[1] = new DoubleSolenoid(PORT_1_CLAW_SOLENOID, PORT_2_CLAW_SOLENOID);
-	Solenoids[2] = new DoubleSolenoid(PORT_1_INTAKE_SOLENOID, PORT_2_INTAKE_SOLENOID);
-	Solenoids[3] = new DoubleSolenoid(PORT_1_DOOR_SOLENOID, PORT_2_DOOR_SOLENOID);
-	Timers[0] = new Timer();
-	Timers[1] = new Timer();
-	Timers[2] = new Timer();
-	Timers[3] = new Timer();
-	AirCompressor = new Compressor();
-	Macro = new MacroRecorder();
+	o_Macro = new Macro();
+	o_Joysticks[0] = new Joystick(PORT_JOYSTICK_1);
+	o_Joysticks[1] = new Joystick(PORT_JOYSTICK_2);
+	o_Compressor = new Compressor();
+	o_Drive = new Drive();
+	o_Intake = new Intake();
+	o_Lift = new Lift();
 }
 
 
 Robot::~Robot(){
-	for(int i = 0;i < NUMBER_OF_JOYSTICKS;i++)
-		delete Joysticks[i];
-
-	for(int i = 0;i < NUMBER_OF_TALONS;i++)
-		delete Talons[i];
-
-	for(int i = 0;i < NUMBER_OF_SOLENOIDS;i++)
-		delete Solenoids[i];
-
-	for(int i = 0;i < NUMBER_OF_SOLENOIDS;i++)
-		delete Timers[i];
-
-	delete AirCompressor;
-	delete Macro;
-}
-
-
-void Robot::RobotInit(){
-	AirCompressor->Start();
-}
-
-
-void Robot::Autonomous(){
-	Macro->Play(AUTONOMOUS_PROGRAM, Talons, Solenoids);
-}
-
-
-void Robot::OperatorControl(){
-	//Start recording macro
-	Macro->StartRecording(false);
-
-	// Variables
-	float y, z, slider;                         // Joystick floats
-	int left_intake_state, right_intake_state;  //Joystick integers
-	bool solenoid_buttons[NUMBER_OF_SOLENOIDS]; // Joystick booleans
-	float right_speed, left_speed;              // Motor speeds
-	int solenoid_state[NUMBER_OF_SOLENOIDS];    // 0 = Off, 1 = Forward, 2 = Reverse
-	bool solenoid_forward[NUMBER_OF_SOLENOIDS], solenoid_button_already_pressed[NUMBER_OF_SOLENOIDS]; // Used to toggle solenoid states
-
-	// Set all solenoids in reverse
-	for(int i = 0;i < NUMBER_OF_SOLENOIDS;i++){
-		Solenoids[i]->Set(DoubleSolenoid::kReverse);
-		solenoid_state[i] = 2;
-		solenoid_forward[i] = false;
-		solenoid_button_already_pressed[i] = false; // These also need to be set before the tele-op loop begins, so I'll just do that here
-	}
-
-	// Record macro step
-	Macro->SaveStep(0, 0, 0, 0, solenoid_state[0], solenoid_state[1], solenoid_state[2], solenoid_state[3]);
-
-	// Set the solenoids back off
-	Wait(SOLENOID_STATE_TIME_DELAY);
-	for(int i = 0;i < NUMBER_OF_SOLENOIDS;i++){
-		Solenoids[i]->Set(DoubleSolenoid::kOff);
-		solenoid_state[i] = 0;
-	}
-
-	// Record macro step
-	Macro->SaveStep(0, 0, 0, 0, solenoid_state[0], solenoid_state[1], solenoid_state[2], solenoid_state[3]);
+	delete o_Macro;
 	
+	for(int i = 0;i < NUMBER_OF_JOYSTICKS;i++)
+		delete o_Joysticks[i];
+
+	delete o_Compressor;
+	delete o_Drive;
+	delete o_Intake;
+	delete o_Lift;
+}
+
+
+void Robot::RobotInit(void){
+	o_Compressor->Start();
+}
+
+
+void Robot::Autonomous(void){
+	o_Macro->Play(0, o_Drive, o_Lift, o_Intake, o_Compressor);
+}
+
+
+void Robot::OperatorControl(void){
+	float y1, y2, left_speed, right_speed, left_intake_state, right_intake_state;
+	bool b_arm, b_claw, b_intake_clamp, b_intake_left_in, b_intake_left_out, b_intake_right_in, b_intake_right_out;
+	
+	o_Macro->StartRecording(false);
+
 	while(IsOperatorControl() && IsEnabled()){
-		// Cache joystick values
-		y = Joysticks[0]->GetY();
-		z = Joysticks[0]->GetZ();
-		slider = Joysticks[0]->GetThrottle();
-		solenoid_buttons[0] = Joysticks[1]->GetRawButton(JOYSTICK_BUTTON_LIFT_SOLENOID);
-		solenoid_buttons[1] = Joysticks[1]->GetRawButton(JOYSTICK_BUTTON_CLAW_SOLENOID);
-		solenoid_buttons[2] = Joysticks[1]->GetRawButton(JOYSTICK_BUTTON_INTAKE_SOLENOID);
-		solenoid_buttons[3] = Joysticks[1]->GetRawButton(JOYSTICK_BUTTON_DOOR_SOLENOID);
-
-		// Calculate motor speeds.
-		right_speed = (1 - ((slider + 1) / 2)) * (y + z); // This math takes the slider's input and converts it from -1 - 1 to 1 - 0, then uses that as a speed multiplier
-		left_speed = -(1 - ((slider + 1) / 2)) * (y - z);
-
-		// Drive
-		for(int i = 2;i < NUMBER_OF_TALONS;i++){
-			if(i < NUMBER_OF_TALONS - 2){
-				Talons[i]->Set(left_speed);
-			}else{
-				Talons[i]->Set(right_speed);
-			}
-		}
-
-		// Left intake motor
-		if(Joysticks[1]->GetRawButton(JOYSTICK_BUTTON_LEFT_INTAKE_MOTOR_IN)){
+		y1 = o_Joysticks[0]->GetRawAxis(AXIS_Y1);
+		y2 = o_Joysticks[0]->GetRawAxis(AXIS_Y2);
+		b_intake_clamp = o_Joysticks[0]->GetRawButton(BUTTON_INTAKE_CLAMP);
+		b_intake_left_in = o_Joysticks[0]->GetRawButton(BUTTON_INTAKE_LEFT_IN);
+		b_intake_left_out = o_Joysticks[0]->GetRawButton(BUTTON_INTAKE_LEFT_OUT);
+		b_intake_right_in = o_Joysticks[0]->GetRawButton(BUTTON_INTAKE_RIGHT_IN);
+		b_intake_right_out = o_Joysticks[0]->GetRawButton(BUTTON_INTAKE_RIGHT_OUT);
+		
+		b_arm = o_Joysticks[1]->GetRawButton(BUTTON_ARM);
+		b_claw = o_Joysticks[1]->GetRawButton(BUTTON_CLAW);
+		
+		left_speed = -y1;
+		right_speed = -y2;
+		
+		if(b_intake_left_in)
 			left_intake_state = 1;
-			Talons[0]->Set(INTAKE_SPEED);
-		}else if(Joysticks[1]->GetRawButton(JOYSTICK_BUTTON_LEFT_INTAKE_MOTOR_OUT)){
+		else if(b_intake_left_out)
 			left_intake_state = 2;
-			Talons[0]->Set(-INTAKE_SPEED);
-		}else{
+		else
 			left_intake_state = 0;
-			Talons[0]->Set(0);
-		}
-
-		// Right intake motor
-		if(Joysticks[1]->GetRawButton(JOYSTICK_BUTTON_RIGHT_INTAKE_MOTOR_IN)){
+		
+		if(b_intake_right_in)
 			right_intake_state = 1;
-			Talons[1]->Set(INTAKE_SPEED);
-		}else if(Joysticks[1]->GetRawButton(JOYSTICK_BUTTON_RIGHT_INTAKE_MOTOR_OUT)){
+		else if(b_intake_right_out)
 			right_intake_state = 2;
-			Talons[1]->Set(-INTAKE_SPEED);
-		}else{
+		else
 			right_intake_state = 0;
-			Talons[1]->Set(0);
-		}
+		
+		o_Drive->Set(left_speed, right_speed);
+		o_Lift->ToggleUpDown(b_arm);
+		o_Lift->ToggleInOut(b_claw);
+		o_Intake->Toggle(b_intake_clamp);
+		o_Intake->Set(left_intake_state, right_intake_state);
 
-		//Solenoid control
-		for(int i = 0;i < NUMBER_OF_SOLENOIDS;i++){
-			if(solenoid_buttons[i] && !solenoid_button_already_pressed[i] && Timers[i]->Get() == 0){ // This section uses logic to toggle between the solenoid being forward or reverse
-				solenoid_button_already_pressed[i] = true;                  // Since all of the solenoids and solenoid variables are arrays, they can be looped
-				if(solenoid_forward[i]){
-					Solenoids[i]->Set(DoubleSolenoid::kReverse);
-					solenoid_forward[i] = false;
-					solenoid_state[i] = 2;
-				}else{
-					Solenoids[i]->Set(DoubleSolenoid::kForward);
-					solenoid_forward[i] = true;
-					solenoid_state[i] = 1;
-				}
-				Timers[i]->Reset();
-				Timers[i]->Start();
-			}else if(!solenoid_buttons[i] && solenoid_button_already_pressed[i]){
-				solenoid_button_already_pressed[i] = false;
-			}
-			if(Timers[i]->Get() > SOLENOID_STATE_TIME_DELAY){ // This section waits for a short amount of time before turning the solenoid off again
-				Solenoids[i]->Set(DoubleSolenoid::kOff);
-				solenoid_state[i] = 0;
-				Timers[i]->Stop();
-				Timers[i]->Reset();
-			}
-		}
+		o_Macro->SaveStep(left_speed, right_speed, b_arm, b_claw, b_intake_clamp, left_intake_state, right_intake_state);
 
-		// Record macro step
-		Macro->SaveStep(left_speed, right_speed, left_intake_state, right_intake_state, solenoid_state[0], solenoid_state[1], solenoid_state[2], solenoid_state[3]);
-
-		//Wait before next cycle
 		Wait(CYCLE_TIME_DELAY);
 	}
 
-	// Stop recording macro
-	Macro->StopRecording();
+	o_Macro->StopRecording();
 }
+
 
 START_ROBOT_CLASS(Robot);
