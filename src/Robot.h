d03f928aaf00f5fe6412ae46dd7ce10c9cfb7724
CatherineHancock <17hancockc@ttsdstudents.org>
2016-01-21 17:42:17 -0800

    Update Robot.h

--- src/Robot.h
+++ src/Robot.h
@@ -1,16 +1,16 @@
-#ifndef ROBOT_H__INCLUDED
-#define ROBOT_H__INCLUDED
+#ifndef ROBOT_H__INCLUDED // see if ROBOT_H__INCLUDED is already defined
+#define ROBOT_H__INCLUDED // define ROBOT_H__INCLUDED
 
 
-#include "WPILib.h"
-#include "Config.h"
-#include "Drive.h"
-#include "Lift.h"
-#include "Macro.h"
+#include "WPILib.h" // include WPILib.h library
+#include "Config.h" // include Config.h library
+#include "Drive.h" // include Drive.h library
+#include "Lift.h" // include Lift.h library
+#include "Macro.h" // include Macro.h library
 
 
 class Robot: public SampleRobot{
-public:
+public: // info that is available to all programs
 	Robot(void);
 	~Robot(void);
 
@@ -18,7 +18,7 @@ public:
 	void Autonomous(void);
 	void OperatorControl(void);
 	
-private:
+private: // info only available for this program
 	Macro* o_Macro;                             // o_ has been appended to the beginning of each object to avoid naming conflicts (o for object)
 	PowerDistributionPanel* o_PowerDist;
 	Joystick* o_Joysticks[NUMBER_OF_JOYSTICKS];
