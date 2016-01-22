740bf2c61a9ea6e9684da35d03034b65092b2532
CatherineHancock <17hancockc@ttsdstudents.org>
2016-01-21 18:57:16 -0800

    Update Drive.cpp

--- src/Drive.cpp
+++ src/Drive.cpp
@@ -4,28 +4,31 @@
 
 
 Drive::Drive(void){ // Drive class
-	LeftMotor1 = new Talon(PORT_LEFT_TALON_1); // replace all of these variables with 'new' constants
-	LeftMotor2 = new Talon(PORT_LEFT_TALON_2);
-	RightMotor1 = new Talon(PORT_RIGHT_TALON_1);
-	RightMotor2 = new Talon(PORT_RIGHT_TALON_2);
-	LeftEncoder = new Encoder(PORT_1_LEFT_ENCODER, PORT_2_LEFT_ENCODER);
-	RightEncoder = new Encoder(PORT_1_RIGHT_ENCODER, PORT_2_RIGHT_ENCODER);
+// Talon is a motor controller
+// Encoder measures how far it has gone
+	LeftMotor1 = new Talon(PORT_LEFT_TALON_1); // creates new variable/constant PORT_LEFT_TALON_1 for object Talon
+	LeftMotor2 = new Talon(PORT_LEFT_TALON_2); // creates new variable/constant PORT_LEFT_TALON_2 for object Talon
+	RightMotor1 = new Talon(PORT_RIGHT_TALON_1); // creates new variable/constant PORT_LEFT_TALON_1 for object Talon
+	RightMotor2 = new Talon(PORT_RIGHT_TALON_2); // creates new variable/constant PORT_RIGHT_TALON_2 for object Talon
+	LeftEncoder = new Encoder(PORT_1_LEFT_ENCODER, PORT_2_LEFT_ENCODER); // creates new variable/constant PORT_1_LEFT_ENCODER and PORT_2_LEFT_ENCODER for object Talon
+	RightEncoder = new Encoder(PORT_1_RIGHT_ENCODER, PORT_2_RIGHT_ENCODER); // creates new variable/constant PORT_1_RIGHT_ENCODER and PORT_1_RIGHT_ENCODER for onject Encoder
 
 	distance_since_last = 0;
 }
 
 
 Drive::~Drive(void){ // destroy Drive
-	delete LeftMotor1; // delete all old variables
-	delete LeftMotor2;
-	delete RightMotor1;
-	delete RightMotor2;
-	delete LeftEncoder;
-	delete RightEncoder;
+	// delete all old variables to not waste memory
+	delete LeftMotor1; // delete LeftMotor1 variable
+	delete LeftMotor2; // delete LeftMotor2 variable
+	delete RightMotor1; // delete RightMotor1 variable
+	delete RightMotor2; // delete RightMotor2 variable
+	delete LeftEncoder; // delete LeftEncoder variable
+	delete RightEncoder; // delete RightEncoder variable
 }
 
 
-int Drive::Set(float left_speed, float right_speed){ // Set function
+int Drive::Set(float left_speed, float right_speed){ // function to set left speed and right speed
 	distance_since_last = 0;
 
 	LeftMotor1->Set(left_speed); // The right motors are reversed so that passing two positive values to this function will result in the robot moving forward
@@ -33,5 +36,5 @@ int Drive::Set(float left_speed, float right_speed){ // Set function
 	RightMotor1->Set(-right_speed);
 	RightMotor2->Set(-right_speed);
 
-	return distance_since_last;
+	return distance_since_last; // return info about back to computer
 }
